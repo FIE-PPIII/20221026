1fd687b3b6eef391a5458390d83a4a0ea5ea44e6
Juan Bautista Nemi <80529710+jnemi2@users.noreply.github.com>
2022-10-26 12:39:18 -0300

    ADD - List by value

--- main.cpp
+++ main.cpp
@@ -1,10 +1,38 @@
 #include <iostream>
+#include <list>
 
 using namespace std;
 
+class MyClass{
+private:
+    int x;
+public:
+    MyClass(int x);
+    
+    MyClass(const MyClass& old)
+    {
+        std::cout << "Copying... "<<&old<<" into "<<this<<"\n";
+        *this = old;
+    }
+    
+    ~MyClass();
+};
+
+MyClass::MyClass(int x){
+    this->x = x;
+    cout<<"Class "<<this->x<<" at "<<this<<" was created"<<endl;
+}
+
+MyClass::~MyClass(){
+    cout<<"Class "<<this->x<<" at "<<this<<" has been destroyed"<<endl;
+}
+
 int main()
 {
-    cout<<"Hello, world!"<<endl;
-
+    list<MyClass> myList;
+    myList.push_back(MyClass(2));
+    myList.push_back(MyClass(7));
+    for (auto it = myList.begin(); myList.end() != it; it++)
+        cout<<&(*it)<<endl;
     return 0;
 }
